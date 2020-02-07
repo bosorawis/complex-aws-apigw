793aaf72d83f02c0a92fa8cfd5159645bc485cbd
dihmuzikien <turbo2547@hotmail.com>
2020-02-06 21:13:07 -0800

    qwer
    Signed-off-by: dihmuzikien <turbo2547@hotmail.com>

--- function/pingpong/main.go
+++ function/pingpong/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -17,14 +18,31 @@ var (
 	ErrNon200Response = errors.New("Non 200 Response found")
 )
 
-type MSG struct {
-	Message string `json:"message"`
+type Input struct {
+	a   int    `json:"a"`
+	b   int    `json:"b"`
+	ops string `json:"ops"`
+}
+type Output struct {
+	value int `json:"value"`
 }
 
-
-func handler(request MSG) (MSG, error) {
+func handler(request Input) (Output, error) {
 	fmt.Printf("Received: %s\n", request.Message)
-	return MSG{Message: fmt.Sprintf("pong - %s", request.Message)}, nil
+	switch request.ops {
+	case "+":
+		return Output{value: a + b}, nil
+	case "-":
+		return Output{value: a - b}, nil
+	case "*":
+		return Output{value: a * b}, nil
+	case "/":
+		return Output{value: a / b}, nil
+	case "^":
+		return Output{value: a ^ b}, nil
+	default:
+		return nil, errors.New("Unknown Operator %w")
+	}
 }
 
 func main() {
